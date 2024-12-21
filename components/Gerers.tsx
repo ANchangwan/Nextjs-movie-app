434b617174caa5b972bbbf419016520bf3ba30db
waney <ssdii66@gmail.com>
2024-12-21 22:16:52 +0900

    movie/[id] 페이지에서 장르의 글자수가 많아질 때 height 달라지는 문제 해결

--- components/Gerers.tsx
+++ components/Gerers.tsx
@@ -1,10 +1,7 @@
-
-
-
-export default function Gerers({name}:{name:string}) {
+export default function Gerers({ name }: { name: string }) {
     return (
-        <div className="p-2 bg-gray-500 rounded-xl flex justify-center items-center mr-2 ring-1 ring-amber-50 outline-amber-50">
-            <h2 className="font-semibold uppercase text-xs">{name}</h2>
+        <div className="p-2 max-h-8 bg-gray-500 rounded-xl flex justify-center items-center mr-2 ring-1 ring-amber-50 outline-amber-50">
+            <h2 className="font-semibold uppercase text-[11px] text-center whitespace-nowrap overflow-hidden text-ellipsis">{name}</h2>
         </div>
     )
 }
